2ece8c2d9f5ad9912d2631ed78cba3ccfa1893e8
Sean Kerry <spkerry@live.com>
2017-06-08 13:49:56 -0500

    finished class 8 prep by adding edit action and form

---
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CheeseMVC.Controllers
+{
+    public class CheeseController : Controller
+    {
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Cheese> cheeses = CheeseData.GetAll();
+
+            return View(cheeses);
+        }
+
+        public IActionResult Add()
+        {
+            AddCheeseViewModel addCheeseViewModel = new AddCheeseViewModel();
+            return View(addCheeseViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCheeseViewModel addCheeseViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add the new cheese to my existing cheeses
+                Cheese newCheese = new Cheese
+                {
+                    Name = addCheeseViewModel.Name,
+                    Description = addCheeseViewModel.Description,
+                    Type = addCheeseViewModel.Type
+                };
+
+                CheeseData.Add(newCheese);
+
+                return Redirect("/Cheese");
+            }
+            return View(addCheeseViewModel);
+            
+        }
+
+        public IActionResult Remove()
+        {
+            ViewBag.title = "Remove Cheeses";
+            ViewBag.cheeses = CheeseData.GetAll();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Remove(int[] cheeseIds)
+        {
+            foreach (int cheeseId in cheeseIds)
+            {
+                CheeseData.Remove(cheeseId);
+            }
+
+            return Redirect("/");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            ViewBag.cheese = CheeseData.GetById(id);
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, string name, string description)
+        {
+            Cheese cheese = CheeseData.GetById(id);
+            cheese.Name = name;
+            cheese.Description = description;
+
+            return Redirect("/");
+        }
+    }
+}
---
+++ CheeseMVC/Models/Cheese.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Models
+{
+    public class Cheese
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public CheeseType Type { get; set; }
+
+        public int CheeseId { get; set; }
+        private static int nextId = 1;
+
+        public Cheese()
+        {
+            CheeseId = nextId;
+            nextId++;
+        }
+    }
+}
